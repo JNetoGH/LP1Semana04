189761c99b9a0b2fbafec085158a09ca0758dd1c
JNeto <joaomendes15@gmail.com>
2023-03-23 23:50:48 +0000

    Ex4 done

--- Power2Method/Program.cs
+++ Power2Method/Program.cs
@@ -7,14 +7,22 @@ namespace Power2Method
         
         static void Main(string[] args)
         {
+            Console.WriteLine("until 5: ");
             PowersOf2Until5();
-            PowersOf2Until5();
+            
+            Console.WriteLine("\nuntil 3: ");
+            PowersOf2UntilN(3);
+            
+            Console.WriteLine("\nuntil 2: ");
+            PowersOf2UntilN(2);
         }
         
-        private static void PowersOf2Until5()
+        // 0b_1 << 5 == ob_000001 == 35(10)
+        private static void PowersOf2Until5() => PowersOf2UntilN(5);
+        
+        private static void PowersOf2UntilN(int n)
         {
-            // 0b_1 << 5 == ob_000001 == 35(10)
-            for (int i = 1; i <= (1 << 5); i = i << 1)
+            for (int i = 1; i <= (1 << n); i = i << 1)
                 Console.WriteLine(i);
         }
         
